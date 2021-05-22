d6831e02547e65d47e3bfd05a265fa3802df9b60
MaganaOscar <oscmag13@gmail.com>
2021-05-21 18:16:59 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function playVid() {
+    var vid = document.getElementById("vidPlayer");
+    vid.play();
+}
+function stopVid() {
+    var vid = document.getElementById("vidPlayer");
+    vid.pause();
+}
